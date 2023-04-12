3183730a54ad665d88abcc9cdb4cbf82e33b563c
= <basile08@hotmail.fr>
2023-04-13 00:26:28 +0200

    WIP: Ajout/modif de sessions de formation

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Session;
+use App\Form\SessionType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class SessionController extends AbstractController
 {
@@ -15,4 +21,47 @@ class SessionController extends AbstractController
             'controller_name' => 'SessionController',
         ]);
     }
+
+
+    // Gère l'affichage du form d'ajout/modification MAIS GERE AUSSI l'envoi du form (if isSubmitted  ou juste affichage form )
+    #[Route('/session/{id}/edit', name: 'app_editSession')]
+    #[Route('/session/add', name: 'app_addSession')]
+    public function add(EntityManagerInterface $entityManager, Session $session = null, Request $request): Response  {
+
+        // Méthode rapide pour savoir si User connecté
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // On vérifie dans quel cas on est (création ou modification de l'entité)
+        if(!$session) {
+            $session = new Session();
+        }
+
+        $form = $this->createForm(SessionType::class, $session);
+        $form -> handleRequest($request);
+
+        // Vérifs/Filtres
+        if($form->isSubmitted()) {
+            if($form->isValid()) {
+
+                // Hydrataion "Entreprise $entreprise" a partir des données du form
+                $session = $form->getData();
+                // Equivalent au prepare et execute PDO (persist() avant si ajout en BDD !)
+                $entityManager->persist($session);
+                $entityManager->flush();
+
+                // On récupère l'id de la formation "mère" pour rediriger vers Détail formation
+                return $this->redirectToRoute('app_trainingDetail', array('id' => $session->getTraining()->getId()) );
+            }
+        }
+
+
+        // View qui affiche le formuaire d'ajout
+        return $this->render('session/add.html.twig', [
+            'formAddSession' => $form->createView(),
+            // 'trainingId' => $session->getTraining()->getId(),
+            // Si y'a Id c'est qu'on modifie (sinon renvoie false, = création), pour le titre de la page
+            'edit' => $session->getId()
+        ]);
+    }
+
 }
---
+++ src/Form/SessionType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Training;
+use App\Entity\Trainer;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('begin_date', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('end_date', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('nbr_places', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+
+            // ->add('trainees')
+
+            ->add('trainer')
+            // Selection parmies les entités formateurs(trainers) 
+            ->add('trainer', EntityType::class, [
+                'class' => Trainer::class,
+                // 'choice_label' => 'title',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+
+
+            // Selection parmies les entités formation(trainings) DESACTIVE car on est déjà sur la page de la formation "mère"
+            // Problème: en mettant training en disable, la value est nulle (essayer readonly ou donner la value par defaut)
+            ->add('training', EntityType::class, [
+                'class' => Training::class,
+                'choice_label' => 'title',
+                // 'readonly' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    // 'disabled' => true,
+                    
+                    // 'value' => 
+                ]
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
