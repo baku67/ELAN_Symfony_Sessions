ea55a0b1ed0d10a7e16ab8379b95f53c2cac4d18
= <basile08@hotmail.fr>
2023-04-12 16:57:17 +0200

    (WIP) Listing + details + ajout : Formations

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -2,17 +2,91 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Training;
+use App\Form\TrainingType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrainingController extends AbstractController
 {
+
     #[Route('/training', name: 'app_training')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+        // Méthode rapide pour savoir si User connecté
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        // if($this->getUser()) {}
+
+        $trainingRepo = $entityManager->getRepository(Training::class);
+
+        $trainingList = $trainingRepo->findBy([], ['title' => 'ASC']);
+
         return $this->render('training/index.html.twig', [
-            'controller_name' => 'TrainingController',
+            'trainings' => $trainingList,
         ]);
     }
+
+
+    // Bouton pour Edit -> route juste en dessous
+    #[Route('/trainingDetail/{id}/view', name: 'app_trainingDetail')]
+    public function trainingDetail(EntityManagerInterface $entityManager, int $id): Response {
+
+        // Méthode rapide pour savoir si User connecté
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $trainingRepo = $entityManager->getRepository(Training::class);
+
+        $trainingData = $trainingRepo->find($id);
+
+        return $this->render('training/trainingDetail.html.twig', [
+            'training' => $trainingData,
+        ]);
+    }
+
+
+
+        // Gère l'affichage du form d'ajout/modification MAIS GERE AUSSI l'envoi du form (if isSubmitted  ou juste affichage form )
+        #[Route('/training/{id}/edit', name: 'app_editTraining')]
+        #[Route('/training/add', name: 'app_addTraining')]
+        public function add(EntityManagerInterface $entityManager, Training $training = null, Request $request): Response  {
+    
+            // Méthode rapide pour savoir si User connecté
+            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    
+            // On vérifie dans quel cas on est (création ou modification de l'entité)
+            if(!$training) {
+                $training = new Training();
+            }
+    
+            $form = $this->createForm(TrainingType::class, $training);
+            $form -> handleRequest($request);
+    
+            // Vérifs/Filtres
+            if($form->isSubmitted()) {
+                if($form->isValid()) {
+    
+                    // Hydrataion "Entreprise $entreprise" a partir des données du form
+                    $training = $form->getData();
+                    // Equivalent au prepare et execute PDO (persist() avant si ajout en BDD !)
+                    $entityManager->persist($training);
+                    $entityManager->flush();
+    
+                    return $this->redirectToRoute('app_trainingDetail', array('id' => $training->getId()) );
+                }
+            }
+    
+    
+            // View qui affiche le formuaire d'ajout
+            return $this->render('training/add.html.twig', [
+                'formAddTraining' => $form->createView(),
+                // Si y'a Id c'est q'on modifie (sinon renvoie false, = création), pour le titre de la page
+                'edit' => $training->getId()
+            ]);
+        }
+    
 }
---
+++ src/Form/TrainingType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Training;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrainingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Training::class,
+        ]);
+    }
+}
