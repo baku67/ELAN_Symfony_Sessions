4303ed7d7459ebe48ffb47d63c792bfedae1100f
= <basile08@hotmail.fr>
2023-04-13 22:11:35 +0200

    Fix unsubscribe + ajout Subscribe TraineeSession

--- src/Controller/TraineeSessionController.php
+++ src/Controller/TraineeSessionController.php
@@ -33,14 +33,39 @@ class TraineeSessionController extends AbstractController
         $traineeRepo = $entityManager->getRepository(Trainee::class);
         $sessionRepo = $entityManager->getRepository(Session::class);
 
+        $trainee = $traineeRepo->find($id);
+        $session = $sessionRepo->find($idSession);
 
+        // Marche mais Pas la bonne méthode et + compliqué
+        // $traineeSessionToRemove = $traineeSessionRepo->findOneBy(['trainee' => $trainee->getId(), 'session' => $session->getId()]);
+        // $traineeSessionRepo->remove($traineeSessionToRemove, true);
+
+        $session->removeTrainee($trainee);
+        // $entityManager->persist($session);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_sessionDetail', array('id' => $idSession) );
+
+    }
+
+
+    #[Route('/traineeSession/add/{id}/{idSession}', name: 'app_addtraineeSession')]
+    public function add(EntityManagerInterface $entityManager, int $id, int $idSession): Response
+    {
+
+        // Méthode rapide pour savoir si User connecté
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $traineeSessionRepo = $entityManager->getRepository(TraineeSession::class);
+        $traineeRepo = $entityManager->getRepository(Trainee::class);
+        $sessionRepo = $entityManager->getRepository(Session::class);
 
         $trainee = $traineeRepo->find($id);
         $session = $sessionRepo->find($idSession);
 
-        $traineeSessionToRemove = $traineeSessionRepo->findOneBy(['trainee' => $trainee->getId(), 'session' => $session->getId()]);
-
-        $traineeSessionRepo->remove($traineeSessionToRemove, true);
+        $session->addTrainee($trainee);
+        $entityManager->persist($session);
+        $entityManager->flush();
 
         return $this->redirectToRoute('app_sessionDetail', array('id' => $idSession) );
 
